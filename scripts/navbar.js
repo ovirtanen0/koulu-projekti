c5b9425280631f6dbd430eb88bf16dae6d6dec93
Onni Virtanen <onni.virtanen15@gmail.com>
2026-05-11 13:28:00 +0300

    login modal

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,8 @@
 navbar = document.getElementById("navbar");
 navbarOpen = document.getElementById("navbarOpen");
 navbarClose = document.getElementById("navbarClose");
-
+modalOpen = document.getElementById("modalOpen");
+modalClose = document.getElementById("modalClose");
 
 navbarOpen.addEventListener("click", () => {
     navbar.style.right = "0";
@@ -12,3 +13,13 @@ navbarClose.addEventListener("click", () => {
     navbar.style.right = "-350px";
     navbar.style.boxShadow = "none";
 });
+
+modalOpen.addEventListener("click", () => {
+    navbar.style.boxShadow = "0 0 0 max(100vh, 100vw) rgba(0, 0, 0, .25)";
+    document.getElementById("modallogin").style.display = 'block';
+});
+
+modalClose.addEventListener("click", () => {
+    navbar.style.boxShadow = "none";
+    document.getElementById('modallogin').style.display = 'none';
+})
