f2f7cc4b97f1546aec243a391f4d87e1103800a9
Onni Virtanen <onni.virtanen15@gmail.com>
2026-05-13 13:23:56 +0300

    kirjautumis-ikkuna styling

--- scripts/loginModal.js
+++ scripts/loginModal.js
@@ -4,11 +4,10 @@ modalClose = document.getElementById("modalClose");
 
 modalOpen.addEventListener("click", () => {
     navbar.style.boxShadow = "0 0 0 max(100vh, 100vw) rgba(0, 0, 0, .25)";
-    modal.classList.toggle('open');
-    modal.style.borderRadius = "9px";
+    modal.classList.toggle("open");
 });
 
 modalClose.addEventListener("click", () => {
     navbar.style.boxShadow = "none";
-    modal.classList.toggle('open');
-})
+    modal.classList.toggle("open");
+});
